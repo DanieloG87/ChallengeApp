ee8ec7671e675380b5b30dda31d41eb0a38c4747
Degie87 <gamerdg87@gmail.com>
2024-03-04 14:13:09 +0100

    Zmienne podstawowe i operacje

---
+++ ChallengeApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+var name = "Ewa";
+var gender = "kobieta";
+var age = 30;
+
+if (gender == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej lat 30");
+}
+else
+{
+    if (name == "Ewa" && age == 30)
+    {
+        Console.WriteLine("Ewa, lat 30");
+    }
+    else
+    {
+        if (gender == "mężczyzna" && age < 18)
+        {
+            Console.WriteLine("Niepełnoletni mężczyzna");
+        }
+        else
+        {
+            Console.WriteLine("");
+        }
+    }
+}
